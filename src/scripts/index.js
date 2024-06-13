adcd94045ea0aa841a6a73f8f1b0afb4227adbec
DanielDanzo <sempapadaniel123@gmaiil.com>
2024-06-13 18:41:49 +0200

    just added comments

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,10 @@ const submit_btn = document.getElementById("submit-btn");
 
 
 submit_btn.addEventListener("click", ()=>{
+    //Show registration form if user is not registered
+
+    //Show password input text if user is already registered
+
+    //Otherwise it means user is waiting for approval from the admin which is you
     
-    console.log(email.value);
 });
