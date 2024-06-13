209a9ff6d679b3c1ecec5e3ea85f4aae7f57178b
DanielDanzo <sempapadaniel123@gmaiil.com>
2024-06-13 18:38:15 +0200

    next commit

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+var email = document.getElementById("email-input");
+const submit_btn = document.getElementById("submit-btn");
+
+
+
+submit_btn.addEventListener("click", ()=>{
+    
+    console.log(email.value);
+});
